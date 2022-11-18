75c698661a8d662c077d855b91fe53e23be77020
Ianniu123 <985683179yzy@gmail.com>
2022-11-17 23:54:16 -0500

    Need to fix moving and add score and eating food

--- Snake.py
+++ Snake.py
@@ -1,39 +1,77 @@
 import pygame
 import random
 
+class player():
+    player_x = 400
+    player_y = 300
+    player_x_dir = 20
+    player_y_dir = 0
+
 game_on = True
 
 screen = pygame.display.set_mode((800, 600))
 game_on = True
+snack_exist = False
 
-player_x = 400
-player_y = 300
+score = 0
 
 food_x = 0
 food_y = 0
 
-def food():
-    pygame.draw.rect(screen, (255,0,0), (x, y, 20, 20), 5)
+clock = pygame.time.Clock()
 
+def collision():
+    if player.player_x - 20 < 0 or player.player_x + 21 > 800 or player.player_y + 21 > 600 or player.player_y - 20 < 0:
+        return True
 
-
+    return False
 
 while game_on:
+    screen.fill((0,0,0))
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             exit()
-            
-    pygame.draw.rect(screen, (255,0,0), (player_x, player_y, 20, 20), 5)
+    pygame.display.set_caption('Snake')    
 
-    while True:
+    player.player_x += player.player_x_dir
+    player.player_y += player.player_y_dir
+
+    pygame.draw.rect(screen, (255,0,0), (player.player_x, player.player_y, 20, 20), 5)
+
+    while snack_exist == False:
         food_x = random.randint(0, 800)
         food_y = random.randint(0, 600)
-        if (food_x <= player_x - 20 or food_x >= player_x + 20) and (food_y <= player_y - 20 or food_y >= player_y + 20):
+        if (food_x <= player.player_x - 20 or food_x >= player.player_x + 20) and (food_y <= player.player_y - 20 or food_y >= player.player_y + 20):
+            snack_exist = True
             break
-        
+
     pygame.draw.rect(screen, (255,0,0), (food_x, food_y, 20, 20), 5)
-    
+
+    if pygame.key.get_pressed()[pygame.K_RIGHT] == True:
+        player.player_x_dir = 20
+        player.player_y_dir = 0
+    elif pygame.key.get_pressed()[pygame.K_LEFT] == True:
+        player.player_x_dir = -20
+        player.player_y_dir = 0
+    elif pygame.key.get_pressed()[pygame.K_UP] == True:
+        player.player_x_dir = 0
+        player.player_y_dir = -20
+    elif pygame.key.get_pressed()[pygame.K_DOWN] == True:
+        player.player_x_dir = 0
+        player.player_y_dir = 20
+        
+    if collision():
+        player.player_x_dir = 0
+        player.player_y_dir = 0
+
+    if ():
+        score += 1
+        snack_exist = False
+        print("over")
+
     pygame.display.update()
+
+    clock.tick(20)
 
         
 
